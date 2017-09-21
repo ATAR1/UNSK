73060f15ecdea1de96916ab97de3bc81c39b1bfa
EpaneshnikovSA <EpaneshnikovSA@UNSK7>
2017-09-21 15:01:05 +0500

    Revert "пробую систему контроля версий git"
    This reverts commit 251a62c89cd14163695a9495faed2d83fced149f.

--- TestAndTunes/AddCommand.cs
+++ TestAndTunes/AddCommand.cs
@@ -34,7 +34,6 @@ namespace TestAndTunes
             JournalRecord newRecord = new JournalRecord() { Date=StoredDate, Shift =StoredShift, WorkArea = StoredWorkArea };
             _uncheckedRecord.Model = newRecord;
             _ctx.JournalRecords.Add(newRecord);
-            // test
         }
     }
 }
