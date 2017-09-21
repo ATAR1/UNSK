694166e6b1d82a0a46e60f2fe309951cfc039b5d
EpaneshnikovSA <EpaneshnikovSA@UNSK7>
2017-09-21 15:12:16 +0500

    test

--- TestAndTunes/DeleteCommand.cs
+++ TestAndTunes/DeleteCommand.cs
@@ -41,6 +41,7 @@ namespace TestAndTunes
                 _ctx.SaveChanges();
                 _viewModel.RefreshJournalRecords();
                 _viewModel.RefreshTotals();
+                // test 2
             }
         }
     }
